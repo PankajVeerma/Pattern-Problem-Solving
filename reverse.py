72bbf3d64f9d7e4622371d186385f644309679fe
Pankaj Verma <142305123+PankajVeerma@users.noreply.github.com>
2026-01-18 05:17:19 +0000

    reverse string without using list

--- reverse.py
+++ reverse.py
@@ -1,5 +1,10 @@
 string  =input("Enter the string ")
-list1 = []
-for i in range(len(string)-1,-1,-1):
-  list1.append(string[i])
-print("".join(list1))
+# list1 = []
+# for i in range(len(string)-1,-1,-1):
+#   list1.append(string[i])
+# print("".join(list1))
+
+reverse_string=""
+for char in range(len(string)-1,-1,-1):
+    reverse_string+=string[char]
+print("Reverse String",reverse_string )
