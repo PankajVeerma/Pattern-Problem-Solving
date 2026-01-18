2cc13a97be1009ab71c8f6b87a37e8f38922fa59
Pankaj Verma <142305123+PankajVeerma@users.noreply.github.com>
2026-01-18 10:57:19 +0000

    Substring Related Questions

--- WordsInString.py
+++ WordsInString.py
@@ -1,5 +1,4 @@
-def wordCounting(string):
-        #    BruteForce Solution Time Complexity O(n)
+def wordCounting(string):     #    BruteForce Solution Time Complexity O(n)
     i=0
     count=0
     n=len(string)
@@ -27,5 +26,48 @@ def wordCounting(string):
     # return count
 st="India is Great Country"
 print(wordCounting(st))
+
+
+
+def findDuplicateChar(string):
+    dictionary={}
+    value=1
+    for char in string:
+        if char not in dictionary:
+            dictionary[char]=value 
+        else:
+            dictionary[char] = value +1  
+
+    li=[]
+    for key,value in dictionary.items():
+        if value >1:
+            li.append(key)        
+    return li
+string="abcdefasd"
+print(findDuplicateChar(string))
+
+
+# All Substring
+def AllAubstring(string):
+    result=[]
+    for i in range(len(string)):
+        for j in range(i+1, len(string)+1):
+            result.append(string[i:j])
+    return result        
+string="abcdefasd"
+print(AllAubstring(string))  
+
+
+
+# Sub String of Length k
+def K_LenthSubString(string,k):
+    result=[]
+    for char in range(len(string)-k+1): 
+       result.append(string[char:char+k])
+    return result
+string = "abcdefgh"
+k=3
+print(K_LenthSubString(string,k))
+   
 
 
