6c86a50104f01703199f9903bf52792d77e46ee4
Pankaj Verma <142305123+PankajVeerma@users.noreply.github.com>
2026-01-18 05:33:19 +0000

    Palindrom without slic

--- Palindrom.py
+++ Palindrom.py
@@ -1,5 +1,19 @@
-string = input("Enter the string ")
-if string.lower() == string[::-1].lower():
-  print("Given string is Palindrom")
+# string = input("Enter the string ")
+# if string.lower() == string[::-1].lower():
+#   print("Given string is Palindrom")
+# else:
+#   print("Given string is not Palindrom") 
+
+def palindrom(string):
+  result=True
+  for char in range(len(string)//2):
+    if string[char]!=string[len(string)-1-char]:
+       result=False
+  return result
+string = input("Enter the string ")    
+res=palindrom(string)
+
+if res:
+  print("Palindrom")
 else:
-  print("Given string is not Palindrom")  
+  print("Not Palindrom")  
