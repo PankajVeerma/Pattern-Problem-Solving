d08597fc94e0b737ba5140a97835f35cd709b03c
Pankaj Verma <142305123+PankajVeerma@users.noreply.github.com>
2026-01-26 12:56:06 +0000

    Correct code

--- reverse.py
+++ reverse.py
@@ -1,16 +1,20 @@
-string  =input("Enter the string ")
-# list1 = []
-# for i in range(len(string)-1,-1,-1):
-#   list1.append(string[i])
-# print("".join(list1))
+# string  =input("Enter the string ")
+# # list1 = []
+# # for i in range(len(string)-1,-1,-1):
+# #   list1.append(string[i])
+# # print("".join(list1))
 
-reverse_string=""
-for char in range(len(string)-1,-1,-1):
-    reverse_string+=string[char]
-print("Reverse String",reverse_string )
+# reverse_string=""
+# for char in range(len(string)-1,-1,-1):
+#     reverse_string+=string[char]
+# print("Reverse String",reverse_string )
 
 def reverse(string):
     char=list(string)
+    print("char",char)
     for k in range((len(string)//2)+1):
-        char[k],char[len(char)-1] = char[len(char)-1], char[k]
+        char[k],char[len(char)-k-1] = char[len(char)-k-1], char[k]
     return "".join(char)
+
+string="country"
+print(reverse(string))    
